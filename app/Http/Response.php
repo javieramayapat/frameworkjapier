a78cc73c0e1191191c7268d400e7984c4b98180b
Javier Amaya Patricio <javieramayapat@gmail.com>
2021-03-23 23:35:43 -0500

    Configuración de la clase Response.php

---
+++ app/Http/Response.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http;
+
+class Response
+{
+    //La respuesta puede ser de tipo: Array,Json, Pdf
+    protected $view; 
+
+    public function __construct($view)
+    {
+        //home, contact, index
+        $this->view = $view;
+    }
+
+    // Obtener la vista de respuesta
+    public function getView()
+    {
+        return $this->view;
+    }
+
+    public function send()
+    {
+        // Obtengo la vista 
+        $view = $this->getView();
+
+        // Obtener el contendio que se pintara a en la vista.
+        $content = file_get_contents(__DIR__ . "../../views/$view.php");
+
+        // Plantilla base para renderizar contenido.
+        require __DIR__ . "../../views/layout.php";
+    }
+
+}
